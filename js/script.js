9c5fff946b767f3eb9b9e96efcf7a5916c3a4596
EugeniaMarinzalda <eugeniamarinzalda@gmail.com>
2024-04-30 22:12:19 +0200

    add css to branch

--- js/script.js
+++ js/script.js
@@ -5,13 +5,13 @@ const carousel = document.getElementById('carousel');
 
 let currentPosition = 0;
 
-function pressForward(){
+function pressForward() {
     currentPosition -= 100;
-    currentPosition = Math.max(-1300, currentPosition);    
+    currentPosition = Math.max(-1300, currentPosition);
     carousel.style.transform = `translateX(${currentPosition}px)`;
 }
 
-function pressBackward(){
+function pressBackward() {
     currentPosition += 100;
     currentPosition = Math.min(0, currentPosition);
     carousel.style.transform = `translateX(${currentPosition}px)`;
@@ -20,11 +20,11 @@ function pressBackward(){
 
 btnForward.addEventListener('click', pressForward);
 
-btnBackward.addEventListener('click',pressBackward);
+btnBackward.addEventListener('click', pressBackward);
 
-const nav= document.querySelector("#nav");
-const abrir= document.querySelector("#abrir");
-const cerrar= document.querySelector("#cerrar");
+const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
 
 abrir.addEventListener("click", () => {
     nav.classList.add("visible");
@@ -33,4 +33,32 @@ abrir.addEventListener("click", () => {
 cerrar.addEventListener("click", () => {
     nav.classList.remove("visible");
 })
+
+
+function mostrarSucursal() {
+    var sucursalSeleccionada = document.getElementById("sucursal").value;
+    var mapaIframe = document.getElementById("mapa-iframe");
+    var enlaceMapa = document.getElementById("enlace-mapa");
+
+    if (sucursalSeleccionada === "casa-central") {
+        mapaIframe.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3284.653382689082!2d-58.448302224885644!3d-34.58763605664795!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcb5f22f179757%3A0x9c5ca8c26875d076!2sAguirre%202187%2C%20C1414ATG%20Cdad.%20Aut%C3%B3noma%20de%20Buenos%20Aires!5e0!3m2!1ses-419!2sar!4v1713570361895!5m2!1ses-419!2sar";
+        enlaceMapa.href = "https://maps.app.goo.gl/zVLhHd2juFUXkfj49";
+        enlaceMapa.textContent = "Aguirre 2187, CABA";
+    } else if (sucursalSeleccionada === "sucursal-cordoba") {
+        mapaIframe.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d54482.53735502373!2d-64.22936797142032!3d-31.409756531072173!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9432a2a8f14c30ef%3A0x707ff0a3a177b2ff!2sKia%20Ora%20Spa%20Urbano!5e0!3m2!1ses-419!2sar!4v1713570560922!5m2!1ses-419!2sar";
+        enlaceMapa.href = "https://maps.app.goo.gl/f7RG1bsTpqpqxNBN8";
+        enlaceMapa.textContent = "Av. Patria 352, Córdoba";
+    } else if (sucursalSeleccionada === "sucursal-la-rioja") {
+        mapaIframe.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d111204.56512378863!2d-67.02078795664065!3d-29.424626599999975!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9427dbc3cb045ce3%3A0xa2a3947c2e43f551!2sVELACENTER%20by%20Espacio%20Uno!5e0!3m2!1ses-419!2sar!4v1713570695587!5m2!1ses-419!2sar";
+        enlaceMapa.href = "https://maps.app.goo.gl/y8AssbvbtnGvQVUV6";
+        enlaceMapa.textContent = "Ecuador 837, La Rioja";
+    }
+
+}
+
+window.onload = mostrarSucursal;
+
+document.getElementById("sucursal").addEventListener("change", mostrarSucursal);
+
+
 
