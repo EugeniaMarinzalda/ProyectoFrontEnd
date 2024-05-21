a5ae4d215ff9f56755a9ff32e44558bc2916e78a
Lorena <lalimlc@hotmail.com>
2024-05-21 14:29:53 -0300

    cambio en js

--- js/script.js
+++ js/script.js
@@ -279,4 +279,22 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 });
 
+/* Para arreglar el scroll */
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
 
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+
+        if (targetElement) {
+            const headerHeight = document.querySelector('header-container').offsetHeight;
+            const targetPosition = targetElement.offsetTop - headerHeight;
+
+            window.scrollTo({
+                top: targetPosition,
+                behavior: 'smooth'
+            });
+        }
+    });
+});
