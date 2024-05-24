9d8972f20803dbb6717950336373b5ac482a7331
sebas350 <profesebastian8@gmail.com>
2024-05-24 11:49:07 -0300

    api con vue en profesionales

---
+++ js/vue.js
@@ -0,0 +1,30 @@
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+        items:[]
+    };
+  },
+    created(){
+        this.dataFetch();
+    },
+  methods: {
+    
+    async dataFetch() {
+        try {
+           const res =  await fetch("https://sebas350.github.io/api/db.json");
+           if (!res.ok) {
+               throw new Error('mala respuesta de red');    
+           }
+           const data = await res.json();
+           this.items = data;
+        } catch(error) {
+            console.error("Error fetching data:", error);
+          }
+     },
+     backgroundImg(url) {
+         return { backgroundImage: `url(${url})`};
+     }
+   }
+}).mount('#staff');
